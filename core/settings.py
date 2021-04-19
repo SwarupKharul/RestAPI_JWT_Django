6933efdbdffc9ed118291da4cac254072194fbee
Swarup Kharul <swarupkharulsk@gmail.com>
2021-04-19 15:52:12 +0530

    changed settings

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
     'corsheaders',
     'users',
     'djoser',
