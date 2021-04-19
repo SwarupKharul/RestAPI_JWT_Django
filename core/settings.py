808e600b69c4cb319966c931b8a7e6fcc4b815e9
Swarup Kharul <swarupkharulsk@gmail.com>
2021-04-19 15:49:30 +0530

    changed settings

--- core/settings.py
+++ core/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt'
     'corsheaders',
     'users',
     'djoser',
-    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -132,7 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -149,19 +148,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
 ]
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': SECRET_KEY,
-    'VERIFYING_KEY': None,
-    'AUTH_HEADER_TYPES': ('Bearer','JWT',),
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-}
 
 REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from .views import UserProfileListCreateView, userProfileDetailView
 app_name = 'users'
 
 urlpatterns = [
-    path("all-profiles",UserProfileListCreateView.as_view(),name="all-profiles"),
-    path("profile/<int:pk>",userProfileDetailView.as_view(),name="profile"),
+    path("all-profiles/",UserProfileListCreateView.as_view(),name="all-profiles"),
+    path("profile/<int:pk>/",userProfileDetailView.as_view(),name="profile"),
 ]
