77cc17ce5f3ffd623d730b048a27dcd78f1e7932
Swarup Kharul <swarupkharulsk@gmail.com>
2021-04-19 15:15:30 +0530

    jwt

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
     )
 }
 
